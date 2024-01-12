69e8079f4bd982f5f1863e01b01d48e303d085f3
Dragonkjell <packerwittwer@gmail.com>
2024-01-12 15:11:28 -0700

    finished prep 1

---
+++ demo.cs
@@ -0,0 +1,14 @@
+int x = 5
+Console.WriteLine(x);
+Console.Write("No line after this one");
+
+Console.Write("What is your favorite color?");
+string color = Console.ReadLine();
+
+if (x > y)
+{
+    Console.WriteLine("greater");
+}
+
+string school = "Hogwarts";
+Console.WriteLine($"I am studying at {school}");
