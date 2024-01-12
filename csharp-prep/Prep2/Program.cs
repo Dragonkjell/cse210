14cf8f2cf2fce722a1b5890baa9aae3190e54c3c
Dragonkjell <packerwittwer@gmail.com>
2024-01-12 15:52:37 -0700

    did prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        // this is the prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+
+        int grade = int.Parse(gradePercentage);
+
+        string symbol = "";
+
+        if (grade % 10 >=7)
+        {
+            symbol = "+";
+        }
+        else if (grade % 10 <= 3)
+        {
+            symbol = "-";
+        }
+        else if (grade >= 97 || grade <= 59)
+        {
+            symbol = "";
+        }
+        else
+        {
+            symbol = "";
+        }
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{symbol}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You Passed!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass.");
+        }
     }
 }
--- demo.cs
+++ demo.cs
@@ -5,10 +5,31 @@ Console.Write("No line after this one");
 Console.Write("What is your favorite color?");
 string color = Console.ReadLine();
 
-if (x > y)
-{
-    Console.WriteLine("greater");
-}
-
 string school = "Hogwarts";
 Console.WriteLine($"I am studying at {school}");
+
+if (x > y)
+{
+    if (x > z)
+    {
+        Console.WriteLine("greater than");
+    }
+}
+else if (y > y)
+{
+    Console.WriteLine("less than");
+}
+else
+{
+    Console.WriteLine("neither");
+}
+
+// and == &&
+// or == ||
+// not -- !
+
+string valueInText = "42";
+int number = int.Parse(valueInText);
+
+int number = 65;
+string textVersion = number.ToString();
