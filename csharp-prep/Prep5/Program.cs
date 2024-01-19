ff2f351d1bc2ca705a3507f17823ebd861e5b3a4
Dragonkjell <packerwittwer@gmail.com>
2024-01-19 12:21:42 -0700

    finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Diagnostics.Tracing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int favNumber = PromptUserNumber();
+        Console.WriteLine("");
+        DisplayResult(name, favNumber);
+
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squaredNumber = number * number;
+        return squaredNumber;
+    }
+
+    static void DisplayResult(string name, int favNumber)
+    {
+        Console.WriteLine($"Your name is {name}");
+        Console.WriteLine($"Your favorite number is {favNumber}");
+        int squaredNumber = SquareNumber(favNumber);
+        Console.WriteLine($"{name}, your favorite number squared is {squaredNumber}");
     }
 }
