0744806c43e7875a9cf888a38d81498472bf7291
Packer Wittwer <packerwittwer@gmail.com>
2024-02-13 13:36:23 -0700

    message

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,28 +1,15 @@
 public class Reference
 {
     // private string _book;
-    public string _book { get; set; }
-    private int _chapter;
-    private int _verse;
-
-    Reference(string book, int chapter, int verse)
-    {
-    //     _book = book;
-        _chapter = chapter;
-        _verse = verse;
-    }
-    Reference(string verse1, string verse2, string _book, int _chapter)
-    {
-        _wholeReference = _book + " " + _chapter + " " + verse1 + " " + verse2;
-    }
-    Reference(string verse1, string verse2, string verse3, string  _book, int _chapter)
-    {
-         = _book + " " + _chapter + " " + verse1 + " " + verse2 + " " + verse3;
+    public string Book { get; set; }
+    private int Chapter { get; set; }
+    public string Verse { get; set; }
     }
 
+    public string[,] nameHere = {
+        {
+            "1 Nephi 2:23", "And they would have had no achildren; wherefore they would have remained in a state of innocence, having no bjoy, for they knew no misery; doing no good, for they knew no sin."
+        }
+    };
 
-    public string GetReference()
-    {
-        return 
     }
-}
